b489fc36135633530b340f4f95d7ab4b15e428ad
houyi <houyif2@outlook.com>
2017-03-05 23:11:35 +0800

    基本完成，简单滑动事件添加

--- amountselected/src/main/java/houyi/mime/com/amountselected/ScaleView.java
+++ amountselected/src/main/java/houyi/mime/com/amountselected/ScaleView.java
@@ -1,12 +1,15 @@
 package houyi.mime.com.amountselected;
 
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.animation.ValueAnimator;
 import android.content.Context;
 import android.content.res.TypedArray;
 import android.graphics.Canvas;
 import android.graphics.Paint;
 import android.graphics.Rect;
-import android.support.v4.view.MotionEventCompat;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.view.GestureDetector;
 import android.view.MotionEvent;
 import android.view.View;
@@ -27,6 +30,7 @@ import java.util.List;
 public class ScaleView extends View {
     private static final int DEFAULT_MIN_AMOUNT = 1000;
     private static final int DEFAULT_MAX_AMOUNT = 5000;
+    public static final String TAG = "ScaleView";
 
     private int mMinAmount;
     private int mMaxAmount;
@@ -46,13 +50,23 @@ public class ScaleView extends View {
     private float mScaleCellSpace;
     private float mMiddleMarkSize;
     private float mSmallMarkSize;
+    private float startX;
+    private ValueAnimator mValueAnimator;
+    private boolean isCanDrag;
+    private ValueAnimator.AnimatorUpdateListener animatorUpdateListener = new ValueAnimator.AnimatorUpdateListener() {
+        @Override
+        public void onAnimationUpdate(ValueAnimator animation) {
+            mSelectionOffset = (float) animation.getAnimatedValue();
+            postOffset(mSelectionOffset);
+        }
+    };;
 
     public ScaleView(Context context) {
         this(context, null);
     }
 
     public ScaleView(Context context, AttributeSet attrs) {
-        super(context, attrs, 0);
+        this(context, attrs, 0);
     }
 
     public ScaleView(Context context, AttributeSet attrs, int defStyleAttr) {
@@ -71,8 +85,19 @@ public class ScaleView extends View {
     }
 
     private void init() {
+        isCanDrag = true;
         mMinAmount = DEFAULT_MIN_AMOUNT;
         mMaxAmount = mSelectionAmount = DEFAULT_MAX_AMOUNT;
+
+        mValueAnimator = new ValueAnimator();
+        mValueAnimator.setDuration(100);
+        mValueAnimator.addUpdateListener(animatorUpdateListener);
+        mValueAnimator.addListener(new AnimatorListenerAdapter() {
+            @Override
+            public void onAnimationEnd(Animator animation) {
+                isCanDrag = true;
+            }
+        });
 
         mScaleMarkPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
         mScaleMarkPaint.setColor(mScaleMarkColor);
@@ -123,9 +148,9 @@ public class ScaleView extends View {
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
         drawBaseLine(canvas);
-        drawSelectionMark(canvas);
         drawScaleMark(canvas);
         drawScaleNumber(canvas);
+        drawSelectionMark(canvas);
     }
 
     private void drawBaseLine(Canvas canvas) {
@@ -147,15 +172,15 @@ public class ScaleView extends View {
                 + (int) ((mMeasuredWidth / 2 + mSelectionOffset) / mScaleCellSpace) * 100;
 
         for (int amount = mMinAmount, amountCount = 0; amount <= mMaxAmount;
-                amount += 1000, amountCount++) {
+             amount += 1000, amountCount++) {
             //判断金额所在的刻度是否在显示范围内,或者是选中的金额，不在则不显示
             if (amount < theoryMinShowAmount || amount > theoryMaxShowAmount
                     || amount == mSelectionAmount) {
                 continue;
             }
             //计算金额中心位置
-            float amountMiddleX = mMeasuredWidth / 2 - mSelectionOffset +
-                    (mSelectionAmount - amount % 1000 == 0 ? amount : (amount / 1000 + 1) * 1000)
+            float amountMiddleX = mMeasuredWidth / 2 - mSelectionOffset -
+                    (mSelectionAmount - ((amount % 1000 == 0) ? amount : ((amount / 1000 + 1) * 1000)))
                             / 100 * mScaleCellSpace;
             String showAmount = mShowAmountList.get(amountCount);
             Rect showRect = mShowAmountRectList.get(amountCount);
@@ -168,7 +193,7 @@ public class ScaleView extends View {
         //绘制selectionMark右侧刻度
         float middleX = mMeasuredWidth / 2;
         for (int amount = mSelectionAmount + 100, amountCount = 1; amount <= mMaxAmount;
-                amount += 100, amountCount++) {
+             amount += 100, amountCount++) {
             float amountStartX = middleX - mSelectionOffset + mScaleCellSpace * amountCount;
             if (amountStartX > mMeasuredWidth) {
                 break;
@@ -177,7 +202,7 @@ public class ScaleView extends View {
         }
         //绘制selectionMark左侧刻度
         for (int amount = mSelectionAmount, amountCount = 0; amount >= mMinAmount;
-                amount -= 0, amountCount++) {
+             amount -= 100, amountCount++) {
             float amountStartX = middleX - mSelectionOffset - mScaleCellSpace * amountCount;
             if (amountStartX < 0) {
                 break;
@@ -207,16 +232,71 @@ public class ScaleView extends View {
 
     @Override
     public boolean onTouchEvent(MotionEvent event) {
-        MotionEventCompat.getActionMasked(event)
-        return super.onTouchEvent(event);
+        int action = event.getAction();
+        if(isCanDrag) {
+            switch (action) {
+                case MotionEvent.ACTION_DOWN:
+                    startX = event.getX();
+                    break;
+                case MotionEvent.ACTION_MOVE:
+                    if (startX == -1) {
+                        startX = event.getX();
+                    }
+                    float endX = event.getX();
+                    float moveX = endX - startX;
+                    postOffset(mSelectionOffset -= moveX);
+                    startX = endX;
+                    break;
+                case MotionEvent.ACTION_OUTSIDE:
+                case MotionEvent.ACTION_CANCEL:
+                case MotionEvent.ACTION_UP:
+                    moveToRightPosition();
+                    startX = -1;
+                    break;
+            }
+        }
+        return true;
     }
 
-    public void setAmount(int maxAmount, int minAmount) {
-        setAmount(maxAmount, minAmount, maxAmount);
+    private void moveToRightPosition() {
+        isCanDrag =false;
+        if (Math.abs(mSelectionOffset) < mScaleCellSpace / 2) {
+            mValueAnimator.setFloatValues(mSelectionOffset, 0);
+        } else if (mSelectionOffset > 0) {
+            mValueAnimator.setFloatValues(mSelectionOffset, mScaleCellSpace);
+        } else {
+            mValueAnimator.setFloatValues(mSelectionOffset, -mScaleCellSpace);
+        }
+        mValueAnimator.start();
     }
 
-    public void setAmount(int maxAmount, int minAmount, int selectionAmount) {
-        if (mMaxAmount <= mMinAmount || mMinAmount < 0 || mSelectionAmount < mMinAmount
+    @Override
+    protected void onScrollChanged(int l, int t, int oldl, int oldt) {
+        super.onScrollChanged(l, t, oldl, oldt);
+    }
+
+    private void postOffset(float offset) {
+
+        Log.d(TAG, "postOffset: mSelectionOffset" + mSelectionOffset);
+        if (mSelectionOffset >= mScaleCellSpace) {
+            mSelectionOffset = mSelectionOffset - mScaleCellSpace;
+            mSelectionAmount += 100;
+            Log.d(TAG, "postOffset: mSelectionAmount"+mSelectionAmount);
+        }
+        if (mSelectionOffset <= -mScaleCellSpace) {
+            mSelectionOffset = mSelectionOffset + mScaleCellSpace;
+            mSelectionAmount -= 100;
+            Log.d(TAG, "postOffset: mSelectionAmount"+mSelectionAmount);
+        }
+        invalidate();
+    }
+
+    public void setAmount(int minAmount, int maxAmount) {
+        setAmount(minAmount, maxAmount, maxAmount);
+    }
+
+    public void setAmount(int minAmount, int maxAmount, int selectionAmount) {
+        if (mMaxAmount < mMinAmount || mMinAmount < 0 || mSelectionAmount < mMinAmount
                 || mSelectionAmount > mMaxAmount) {
             throw new IllegalArgumentException(
                     "the max amount and the mix amount is incorrect ,now the maxAmount = ["
@@ -226,20 +306,21 @@ public class ScaleView extends View {
         mMaxAmount = maxAmount;
         mMinAmount = minAmount;
         mSelectionAmount = selectionAmount;
+        invalidate();
     }
 
     private GestureDetector.OnGestureListener onGestureListener =
             new GestureDetector.SimpleOnGestureListener() {
                 @Override
                 public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX,
-                        float velocityY) {
+                                       float velocityY) {
                     float x = e2.getX() - e1.getX();
                     float y = e2.getY() - e1.getY();
 
                     if (x > 0) {
-                        doResult(RIGHT);
+//                        doResult(RIGHT);
                     } else if (x < 0) {
-                        doResult(LEFT);
+//                        doResult(LEFT);
                     }
                     return true;
                 }
--- app/src/main/java/houyi/mime/com/amountselectedview/MainActivity.java
+++ app/src/main/java/houyi/mime/com/amountselectedview/MainActivity.java
@@ -1,7 +1,9 @@
 package houyi.mime.com.amountselectedview;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import houyi.mime.com.amountselected.ScaleView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ScaleView scaleView = (ScaleView) findViewById(R.id.scaleview);
+        scaleView.setAmount(0,10000,10000);
     }
 }
